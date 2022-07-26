e93cb0e7c70d094155b1f7b14fae54db01f59ab0
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-07-26 12:05:08 +0600

    Subject Page Design - 26/07/2022

--- assets/js/pages/gallery.js
+++ assets/js/pages/gallery.js
@@ -1,19 +1,18 @@
 /* ------------------------------------------------------------------------------
-*
-*  # Media gallery
-*
-*  Specific JS code additions for Gallery pages
-*
-*  Version: 1.0
-*  Latest update: Aug 1, 2015
-*
-* ---------------------------------------------------------------------------- */
+ *
+ *  # Media gallery
+ *
+ *  Specific JS code additions for Gallery pages
+ *
+ *  Version: 1.0
+ *
+ *  Latest update: 25 July 2022
+ *
+ * ---------------------------------------------------------------------------- */
 
-$(function() {
-
-	// Initialize lightbox
-    $('[data-popup="lightbox"]').fancybox({
-        padding: 3
-    });
-    
+$(function () {
+  // Initialize lightbox
+  $('[data-popup="lightbox"]').fancybox({
+    padding: 3,
+  });
 });
